a9cc28ba0ed69494f043d33cd8948f376df38b13
Hager Sameh <hager_sharaf236@yahoo.com>
2026-06-13 16:29:16 +0300

    navbar

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import Navbar from "@/components/Navbar/Navbar";
+import Navbar from "@/components/Navbar/navbar";
 import SocialSidebar from "@/components/Navbar/SocialSidebar";
 
 export default function MainLayout() {
